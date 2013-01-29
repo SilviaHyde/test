223ff6ebcac81508aaa9606b668fd084865d6519
SilviaHyde <wyelen@gmail.com>
2013-01-29 21:31:50 +0800

    this is a test project
    Change-Id: If3f563c2f1073d86c7b6cd7cd913a0a75c1943fd
    Signed-off-by: SilviaHyde <wyelen@gmail.com>

---
+++ test/test/test.m
@@ -0,0 +1,18 @@
+(* Mathematica Package *)
+
+(* Created by the Wolfram Workbench 2013-1-29 *)
+
+BeginPackage["test`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+myFunc::usage 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
